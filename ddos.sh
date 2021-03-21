dcb52d66a2647d7787f901e5712de214741b261b
jeffypoo <jjones@figure.com>
2021-03-21 13:47:50 -0600

    tweak ddos script

--- ddos.sh
+++ ddos.sh
@@ -8,6 +8,6 @@ URL="http://${HOST}:${PORT}/test"
 # shellcheck disable=SC2034
 for i in {0..1000}
 do
-    curl -s -o /dev/null http://localhost:8080/test -w "%{time_total}\n"
-#    curl -s http://192.168.1.69:8080/test -w "%{time_total}\n"
+#    curl -s -o /dev/null http://localhost:8080/test -w "%{http_code} in %{time_total}\n"
+    curl -s -o /dev/null http://192.168.1.69:8080/test -w "%{http_code} in %{time_total}\n"
 done
