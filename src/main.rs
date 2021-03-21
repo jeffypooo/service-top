6bdc387b2f268c4a12195d463c8556c50724c201
jeffypoo <jjones@figure.com>
2021-03-20 20:57:40 -0600

    something like process list is working

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,17 @@
-use warp::Filter;
-use std::io::Error;
-use prettytable::{
-    Table,
-    Row,
-    Cell
+use futures::{StreamExt, TryStreamExt};
+use futures_timer;
+use heim::{
+    process::{self, Process, ProcessError, ProcessResult},
+    units::{
+        ratio,
+        Ratio,
+    },
 };
+use warp::{Filter};
+use std::time::Duration;
+use heim::process::CpuUsage;
+use std::cmp::Ordering::Equal;
 
-use futures::{
-    executor,
-    stream,
-    Stream,
-    StreamExt
-};
-use heim;
 
 #[tokio::main]
 async fn main() {
@@ -25,13 +24,41 @@ async fn main() {
 }
 
 async fn test() -> Result<String, warp::Rejection> {
+    // let mut proc_stream = heim::process::processes().await.unwrap()
+    //     .filter_map(|proc_res| async move {
+    //         if let Ok(p) = proc_res { Some(p) } else { None }
+    //     })
+    //     .filter_map(|proc| async move {
+    //         if let Ok(name) = proc.name().await { Some(name) } else { None }
+    //     })
+    //     .collect::<Vec<_>>()
+    //     .await;
+    // futures::pin_mut!(proc_stream);
 
-    let f = heim::process::processes()
-        .await
-        .unwrap()
-        .into_future();
+
+    let mut processes = heim::process::processes().await.unwrap()
+        .map_ok(|proc| usage(proc))
+        .try_buffer_unordered(usize::MAX)
+        .filter_map(|res| async move {
+            if let Ok(usage) = res { Some(usage) } else { None }
+        })
+        .collect::<Vec<_>>()
+        .await;
+
+    processes.sort_by(|a: &(String, f32), b: &(String, f32)| {
+        b.1.partial_cmp(&a.1).unwrap_or(Equal)
+    });
 
 
+    Ok(format!("{:#?}", processes))
+}
 
-    Ok(format!("{:#?}", ""))
+async fn usage(process: Process) -> ProcessResult<(String, f32)> {
+    let name = process.name().await?;
+    let usage_1 = process.cpu_usage().await?;
+    futures_timer::Delay::new(Duration::from_millis(100)).await;
+    let usage_2 = process.cpu_usage().await?;
+    let delta = (usage_2 - usage_1).get::<ratio::ratio>() * 100.0;
+
+    Ok((name, delta))
 }
